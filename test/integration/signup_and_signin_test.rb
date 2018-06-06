7c5cc2cbfcdc69e3c070c1759466b825a7cdbf36
twinbird <ixa2063@gmail.com>
2018-06-06 22:56:38 +0900

    add: signup integration test

---
+++ test/integration/signup_and_signin_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class SignupAndSigninTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "signup and transition" do
+    get new_user_registration_path
+    assert_response :success
+
+    post user_registration_path, params: { user: { email: 'test@example.com',
+                                                   password: 'password',
+                                                   password_confirmation: 'password' } }
+    assert_redirected_to mysetting_path
+    follow_redirect!
+    assert_not flash[:notice].empty?
+    assert_select 'a', text: 'サインアウト', count: 1
+  end
+
+  test "signup with wrong info" do
+    get new_user_registration_path
+    assert_response :success
+
+    # wrong email address
+    post user_registration_path, params: { user: { email: 'test@@example.com',
+                                                   password: 'password',
+                                                   password_confirmation: 'password' } }
+    assert_response :success
+    assert_select 'a', text: 'サインアウト', count: 0
+
+    # wrong password confirmation
+    post user_registration_path, params: { user: { email: 'test@example.com',
+                                                   password: 'password',
+                                                   password_confirmation: 'password2' } }
+    assert_response :success
+    assert_select 'a', text: 'サインアウト', count: 0
+
+    # password less than 5 chars
+    post user_registration_path, params: { user: { email: 'test@example.com',
+                                                   password: 'passw',
+                                                   password_confirmation: 'passw' } }
+    assert_response :success
+    assert_select 'a', text: 'サインアウト', count: 0
+  end
+
+end
