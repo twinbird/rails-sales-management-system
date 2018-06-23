fd58a83b30533eda90527c524fe5259102499560
twinbird <ixa2063@gmail.com>
2018-06-23 10:17:48 +0900

    feat: add closing group manage forms

--- app/controllers/closing_groups_controller.rb
+++ app/controllers/closing_groups_controller.rb
@@ -1,10 +1,12 @@
 class ClosingGroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :initialized_user!
   before_action :set_closing_group, only: [:show, :edit, :update, :destroy]
 
   # GET /closing_groups
   # GET /closing_groups.json
   def index
-    @closing_groups = ClosingGroup.all
+    @closing_groups = current_user_company.closing_groups.order(:name).paginate(page: params[:page], per_page: 20)
   end
 
   # GET /closing_groups/1
@@ -14,7 +16,7 @@ class ClosingGroupsController < ApplicationController
 
   # GET /closing_groups/new
   def new
-    @closing_group = ClosingGroup.new
+    @closing_group = current_user_company.closing_groups.build
   end
 
   # GET /closing_groups/1/edit
@@ -24,11 +26,11 @@ class ClosingGroupsController < ApplicationController
   # POST /closing_groups
   # POST /closing_groups.json
   def create
-    @closing_group = ClosingGroup.new(closing_group_params)
+    @closing_group = current_user_company.closing_groups.build(closing_group_params)
 
     respond_to do |format|
       if @closing_group.save
-        format.html { redirect_to @closing_group, notice: 'Closing group was successfully created.' }
+        format.html { redirect_to @closing_group, notice: t('.closing_group_was_successfully_created') }
         format.json { render :show, status: :created, location: @closing_group }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class ClosingGroupsController < ApplicationController
   def update
     respond_to do |format|
       if @closing_group.update(closing_group_params)
-        format.html { redirect_to @closing_group, notice: 'Closing group was successfully updated.' }
+        format.html { redirect_to @closing_group, notice: t('.closing_group_was_successfully_updated') }
         format.json { render :show, status: :ok, location: @closing_group }
       else
         format.html { render :edit }
@@ -56,7 +58,7 @@ class ClosingGroupsController < ApplicationController
   def destroy
     @closing_group.destroy
     respond_to do |format|
-      format.html { redirect_to closing_groups_url, notice: 'Closing group was successfully destroyed.' }
+      format.html { redirect_to closing_groups_url, notice: t('.closing_group_was_successfully_destroyed') }
       format.json { head :no_content }
     end
   end
@@ -64,11 +66,11 @@ class ClosingGroupsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_closing_group
-      @closing_group = ClosingGroup.find(params[:id])
+      @closing_group = current_user_company.closing_groups.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def closing_group_params
-      params.require(:closing_group).permit(:company_information_id, :name)
+      params.require(:closing_group).permit(:name)
     end
 end
