fa80eebac8dbab7b394059648dbdc7186285ca47
twinbird <ixa2063@gmail.com>
2018-07-02 03:45:26 +0900

    apply select2 to prospect

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,5 @@
+$(document).on('turbolinks:load', function() {
+	$('.searchable').select2({
+		language: "ja"
+	});
+});
--- app/assets/javascripts/sales_reports.js
+++
@@ -1,5 +0,0 @@
-$(document).on('turbolinks:load', function() {
-	$('.searchable').select2({
-		language: "ja"
-	});
-});
