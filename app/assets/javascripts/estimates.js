cbd78ce024c89682c35bc3eea1b9dfb86c8bef66
twinbird <ixa2063@gmail.com>
2018-07-04 23:36:45 +0900

    add auto calc prices to estimate form

--- app/assets/javascripts/estimates.js
+++ app/assets/javascripts/estimates.js
@@ -3,6 +3,8 @@ $(document).on('turbolinks:load', function() {
 		$('.searchable').select2({
 			language: 'ja'
 		});
+		$('.estimate-detail-quantity').on('change', calc_price);
+		$('.estimate-detail-unit-price').on('change', calc_price);
 	});
 	var toggle_add_detail_button = function(e, item) {
 		if ($('#estimate_details .nested-fields').length >= 10) {
@@ -18,9 +20,32 @@ $(document).on('turbolinks:load', function() {
 			$('.remove-detail-button').removeClass('disabled');
 		}
 	};
+	var calc_total_amount = function() {
+		var amount = 0;
+		$('.estimate-detail-price').each(function(index) {
+			var txt_amount = $(this).val();
+			amount += Number(txt_amount);
+		});
+		$('#total-amount').text(amount.toString(10));
+	};
+	var calc_price = function() {
+		var rows = $('#estimate_details .nested-fields');
+		rows.each(function(index) {
+			var quantity = $(this).find('.estimate-detail-quantity').val();
+			var unit_price = $(this).find('.estimate-detail-unit-price').val();
+			var price = (Number(quantity) * Number(unit_price));
+			$(this).find('.estimate-detail-price').val(price);
+		});
+		calc_total_amount();
+	};
 	$('#estimate_details').on('cocoon:after-insert', toggle_add_detail_button);
 	$('#estimate_details').on('cocoon:after-insert', toggle_remove_detail_button);
 	$('#estimate_details').on('cocoon:after-remove', toggle_add_detail_button);
 	$('#estimate_details').on('cocoon:after-remove', toggle_remove_detail_button);
+	$('#estimate_details').on('cocoon:after-insert', calc_total_amount);
+	$('#estimate_details').on('cocoon:after-remove', calc_total_amount);
+	$('.estimate-detail-quantity').on('change', calc_price);
+	$('.estimate-detail-unit-price').on('change', calc_price);
 	$('.remove-detail-button').addClass('disabled');
+	calc_total_amount();
 });
