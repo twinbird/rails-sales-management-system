b78b7a55650bffc1ba0fe1adcc2febf802167b21
twinbird <ixa2063@gmail.com>
2018-07-19 07:20:45 +0900

    config for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'rails-sales-management-system.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
